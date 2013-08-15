559eaf5359802c9f300cc06bd8532423ee6d9d88
Deyan Ginev <d.ginev@jacobs-university.de>
2013-08-15 09:24:10 -0400

    better (and more robust) error reporting on fatal failures

--- lib/CorTeX/Service/tex_to_tei_xhtml_v0_1.pm
+++ lib/CorTeX/Service/tex_to_tei_xhtml_v0_1.pm
@@ -14,6 +14,7 @@
 package CorTeX::Service::tex_to_tei_xhtml_v0_1;
 use warnings;
 use strict;
+use Scalar::Util qw/blessed/;
 use base qw(CorTeX::Service);
 use LaTeXML::Converter;
 use LaTeXML::Util::Config;
@@ -31,26 +32,36 @@ sub type {'conversion'}
 sub convert {
   my ($self,%options) = @_;
   # I. Convert to XML
-  my $source = "literal:".$options{workload};
+  my $workload = $options{workload};
+  return {status=>-4,log=>"Fatal:workload:empty No workload provided on input"}
+    unless ($workload && (length($workload)>0));
+  my $source = "literal:".$workload;
   my $converter = LaTeXML::Converter->get_converter($opts);
   $converter->prepare_session($opts);
   my $response = $converter->convert($source);
   my ($latexml_dom, $status, $log) = map { $response->{$_} } qw(result status_code log) if defined $response;
-
+  return {status=>-4,log=>$log."\nFatal:LaTeXML:empty No result created by LaTeXML"}
+    unless (blessed($latexml_dom));
   # Purify
+  # TODO: Append the Purification reports to the $log
   my $purified_dom = LLaMaPUn::Preprocessor::Purify::purify_noparse($latexml_dom,verbose=>0);
+  return {status=>-4,log=>$log."\nFatal:Purify:empty Purification did not return a result."}
+   unless blessed($purified_dom);
   # Tokenize
+  # TODO: Append the Tokenization reports to the $log
   my $marktokens = LLaMaPUn::Preprocessor::MarkTokens->new(document=>$purified_dom,verbose=>0);
   my $tokenized_dom = $marktokens->process_document;
+  return {status=>-4,log=>$log."\nFatal:MarkTokens:empty Tokenization did not return a result."}
+    unless blessed($tokenized_dom);
   # Move to TEI HTML
-  # print STDERR $tokenized_dom->toString(1),"\n\n";
+  # TODO: Append the Post-processing reports to the $log
   my $html_dom = xml_to_TEI_xhtml($tokenized_dom);
+  return {status=>-4,log=>$log."\nFatal:TEI-XHTML:empty Post-processing did not return a result."}
+   unless blessed($html_dom);
 
-  my $result={};
-  $result->{document}=$html_dom->toString(1);
-  $result->{status}= -$status -1; # Adapt to the CorTeX scheme
-  $result->{log} = $log;
-
-  return $result; }
+  return {document=>$html_dom->toString(1),
+          status=>(-$status-1), # Adapt to the CorTeX scheme
+          log=>$log };
+}
 
 1;
