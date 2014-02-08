cf1019020c13f55a9275fbdaebeb09c00358f91d
Deyan Ginev <d.ginev@jacobs-university.de>
2014-02-08 15:43:16 +0100

    official timeout is 300s

--- lib/CorTeX/Service/tex_to_html_v0_1.pm
+++ lib/CorTeX/Service/tex_to_html_v0_1.pm
@@ -21,7 +21,7 @@ use Archive::Zip qw(:CONSTANTS :ERROR_CODES);
 use IO::String;
 
 our $opts=LaTeXML::Common::Config->new(local=>1,whatsin=>'archive',whatsout=>'archive::zip::perl',
-  format=>'html5',mathparse=>'RecDescent',timeout=>120,post=>1,math_formats=>['pmml','cmml'],
+  format=>'html5',mathparse=>'RecDescent',post=>1,math_formats=>['pmml','cmml'],
   preload=>['[ids]latexml.sty'],css=>['http://latexml.mathweb.org/css/external/LaTeXML.css'],
   defaultresources=>0,inputencoding=>'iso-8859-1',timeout=>300);
 $opts->check;
