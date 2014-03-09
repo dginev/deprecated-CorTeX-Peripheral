ebf81d26dbc5faaf26092bfaf367b95b1fd9ce5a
dginev <d.ginev@jacobs-university.de>
2014-03-09 18:34:09 +0100

    now basic support for JSON replies in our C IDF service

--- lib/idf_v0_1.c
+++ lib/idf_v0_1.c
@@ -1,10 +1,20 @@
+/* TO Setup on Ubuntu: 
+  sudo apt-get install uthash-dev libxml++2.6-dev libgearman-dev libjson-c-dev
+*/
 // TO Compile:
-// gcc idf_v0_1.c  -o idf_v0_1.out -lgearman -L/usr/lib/x86_64-linux_gnu/ `xml2-config --cflags` `xml2-config --libs`
+// gcc idf_v0_1.c  -o idf_v0_1.out -ljson-c -lgearman -luthash `xml2-config --cflags` `xml2-config --libs`
+
+// Some everyday C includes
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+// TODO: Do we really need assertions?
 #include <assert.h>
-
+// Hashes
+#include <uthash.h>
+// JSON
+#include <json-c/json.h>
+// Gearman 
 #include <libgearman/gearman.h>
 
 #include <libxml/tree.h>
@@ -14,12 +24,12 @@
 
 void words_from_xpath_nodes(xmlDocPtr doc, xmlNodeSetPtr nodes, FILE* output);
 void *process_document(gearman_job_st *job, void *data, size_t *size, gearman_return_t *ret);
+char *jsonify(char *annotations, char *message, int status, size_t *size);
 
 void *process_document(gearman_job_st *job, void *data, size_t *size, gearman_return_t *ret)
 {
   // Prepare result
-  char *result=(char*)calloc(50,sizeof(char));
-  *size=50;
+  char *message=(char*)calloc(100,sizeof(char));
 
   // Read in a workload from Gearman:
 	size_t workload_size = gearman_job_workload_size(job);
@@ -33,8 +43,8 @@ void *process_document(gearman_job_st *job, void *data, size_t *size, gearman_re
   if (doc == NULL) {
       fprintf(stderr, "Failed to parse workload!\n");
       *ret=GEARMAN_WORK_FAIL;
-      strcpy(result,"Fatal:LibXML:parse Failed to parse workload.");
-      return result; }
+      strcpy(message,"Fatal:LibXML:parse Failed to parse workload.");
+      return jsonify("",message,-4,size); }
 
   xmlXPathContextPtr xpath_context; 
   xmlNodeSetPtr nodeset;
@@ -43,8 +53,8 @@ void *process_document(gearman_job_st *job, void *data, size_t *size, gearman_re
   if(xpath_context == NULL) {
       fprintf(stderr,"Error: unable to create new XPath context\n");
       xmlFreeDoc(doc); 
-      strcpy(result,"Fatal:LibXML:XPath unable to create new XPath context\n");
-      return result; }
+      strcpy(message,"Fatal:LibXML:XPath unable to create new XPath context\n");
+      return jsonify("",message,-4,size); }
 
   /* Register XHTML namespace */
   xmlXPathRegisterNs(xpath_context,  BAD_CAST "xhtml", BAD_CAST "http://www.w3.org/1999/xhtml");
@@ -56,8 +66,8 @@ void *process_document(gearman_job_st *job, void *data, size_t *size, gearman_re
       fprintf(stderr,"Error: unable to evaluate xpath expression \"%s\"\n", xpath);
       xmlXPathFreeContext(xpath_context); 
       xmlFreeDoc(doc); 
-      strcpy(result,"Fatal:LibXML:XPath unable to evaluate xpath expression\n");
-      return result; }
+      strcpy(message,"Fatal:LibXML:XPath unable to evaluate xpath expression\n");
+      return jsonify("",message,-4,size); }
 
   /* Print results */
   words_from_xpath_nodes(doc, xpath_result->nodesetval, stdout);
@@ -70,8 +80,8 @@ void *process_document(gearman_job_st *job, void *data, size_t *size, gearman_re
   xmlCleanupParser();
 
   *ret=GEARMAN_SUCCESS;
-  strcpy(result,"work completed");
-  return result; }
+  strcpy(message,"work completed");
+  return jsonify("",message,-1,size); }
 
 int main(int args,char* argv[]) {
   gearman_worker_st worker;
@@ -86,6 +96,25 @@ int main(int args,char* argv[]) {
 
 
 
+struct word_hash_element {
+      const char *word; /* we'll use this field as the key */
+      int count;             
+      UT_hash_handle hh; /* makes this structure hashable */
+};
+
+void record_word(struct word_hash_element *hash, const char *word) {
+    struct word_hash_element *w;
+    HASH_FIND_STR(hash, word, w);  /* word already in the hash? */
+    if (w==NULL) {
+      w = (struct word_hash_element*)malloc(sizeof(struct word_hash_element));
+      w->word = word;
+      w->count = 1;
+      HASH_ADD_KEYPTR( hh, hash, w->word, strlen(w->word), w ); }
+    else {
+      // Already exists, just increment the counter:
+      w->count++; } }
+
+
 /**
  * print_xpath_nodes:
  * @nodes:    the nodes set.
@@ -94,10 +123,13 @@ int main(int args,char* argv[]) {
  * Prints the @nodes content to @output.
  */
 void words_from_xpath_nodes(xmlDocPtr doc, xmlNodeSetPtr nodes, FILE* output) {
+
+  // Prepare counts hash
+  struct word_hash_element *w, *tmp, *word_counts = NULL;
+
   xmlNodePtr cur;
   int size;
   int i;
-  
   assert(output);
   size = (nodes) ? nodes->nodeNr : 0;
   for(i = 0; i < size; ++i) {
@@ -105,8 +137,30 @@ void words_from_xpath_nodes(xmlDocPtr doc, xmlNodeSetPtr nodes, FILE* output) {
     if (nodes->nodeTab[i]->type == XML_ELEMENT_NODE) {
       cur = nodes->nodeTab[i];        
       xmlChar *word = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
-      fprintf(stderr, "word: %s\n", word );
+      fprintf(stderr,"word: %s\n", word);
+      record_word(word_counts, (const char*)word);
       xmlFree(word);
     }
   }
+
+  /* free the hash table contents */
+  HASH_ITER(hh, word_counts, w, tmp) {
+    fprintf(stderr, "are we iterating?\n");
+    fprintf(stderr,"word_counts{%s} = %d\n", w->word, w->count);
+    HASH_DEL(word_counts, w);
+    free(w);
+  }
 }
+
+char* jsonify(char *annotations, char *message, int status, size_t *size) {
+  json_object *response = json_object_new_object();
+  json_object *json_log = json_object_new_string(message);
+  json_object *json_status = json_object_new_int(status);
+  json_object *json_annotations = json_object_new_string(annotations);
+
+  json_object_object_add(response,"annotations", json_annotations);
+  json_object_object_add(response,"log", json_log);
+  json_object_object_add(response,"status", json_status);
+  const char* string_response = json_object_to_json_string(response);
+  *size = strlen(string_response);
+  return string_response; }
