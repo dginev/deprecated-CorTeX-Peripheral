fabfad3c234206d1568def688e15bc697ae668fe
Deyan Ginev <d.ginev@jacobs-university.de>
2014-03-21 12:27:04 +0100

    comments to the defmock spotter

--- lib/CorTeX/Service/defmock_spotter_v0_1.pm
+++ lib/CorTeX/Service/defmock_spotter_v0_1.pm
@@ -26,21 +26,25 @@ sub analyze {
   my $options = $workload && eval {decode_json($workload);};
   my $status = -4; # Fatal unless we succeed
   my $log; # TODO: Any messages?
+ 
+  # 1. Obtain the input data and do basic error handling:
   my $document = $options && (ref $options) && $options->{document};
   return {status=>-4,log=>"Fatal:workload:empty No workload provided on input"}
     unless ($document && (length($document)>0));
   my $entry = $options->{entry};
   return {status=>-4,log=>"Fatal:entry:empty No entry name provided on input"}
     unless ($entry && (length($entry)>0));
-  # Annotate with number of words in document
+
+  # 2. Now, obtain a LibXML DOM of the document string
   my $parser=XML::LibXML->new();
   $parser->load_ext_dtd(0);
   my $workload_dom = $parser->parse_string($document);
-  #Get an XPath context
+  # 3. Get an XPath context
   my $xpc = XML::LibXML::XPathContext->new($workload_dom);
   my @definition_nodes = $xpc->findnodes('//*[local-name()="span" and @class="ltx_definition"]');
   my $definition_count = scalar(@definition_nodes);
   my $result={};
+  # 4. Annotate with number of words in document
   $result->{annotations}=<<"EOL";
 <rdf:RDF
   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
